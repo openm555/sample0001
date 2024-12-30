0bf70c45cd1984f8758b55aab720c378a8e48574
openm555 <sr50g0329@gmail.com>
2024-12-30 10:24:33 +0900

    test commit

---
+++ src/test/java/com/example/sample0001/sample0001/Sample0001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample0001.sample0001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample0001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
